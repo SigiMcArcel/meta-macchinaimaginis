afd60afac2095e0392a417105aedb90d547f61e9
all <marcel.siegwart@gmx.ch>
2024-10-25 23:23:46 +0200

    add sounds for twittermachine

--- buildtools/setup.sh
+++ buildtools/setup.sh
@@ -15,10 +15,12 @@ BUILD_FOLDER="$ROOT_FOLDER_PREFIX/rpi64-build"
 
 # create build folder
 mkdir -p "$BUILD_FOLDER"
-# copy conf and bblayer
-cp "$PWD/bblayers.conf" "$BUILD_FOLDER"
-cp "$PWD/local.conf" "$BUILD_FOLDER"
 
 # jump into poky and call oe-init-build.env
 cd "$ROOT_FOLDER_PREFIX"source/poky
 source oe-init-build-env ../../rpi64-build
+
+# copy conf and bblayer
+cp "../source/meta-macchinaimaginis/buildtools/bblayers.conf" "conf/"
+cp "../source/meta-macchinaimaginis/buildtools/local.conf" "conf/"
+
